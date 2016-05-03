fc93a977738f1c5c24c35c75f95bdbe16703464c
vinoscript <wstikk@gmail.com>
2016-05-03 23:20:27 +0000

    display all contacts and create contact partially working

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,7 @@ end
 get '/contacts' do
   Contact.all.to_json
 end
+
+post '/contacts' do
+  Contact.create(firstname: params[:firstname], lastname: params[:lastname], email: params[:email])
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,6 +1,13 @@
 $(function() {
 
   var $contacts = $('#contacts');
+  var $firstname = $('#firstname');
+  var $lastname = $('#lastname');
+  var $email = $('#email');
+
+  function addContact(contact){
+    $contacts.append('<li>' + contact.id + ': ' + contact.firstname + ' ' + contact.lastname + '; ' + contact.email + '</li>');
+  }
 
   $.ajax({
     type: 'GET',
@@ -8,9 +15,35 @@ $(function() {
     url: '/contacts',
     success: function(contacts){
       $.each(contacts, function(i, contact){
-        $contacts.append('<li>' + contact.id + ': ' + contact.firstname + ' ' + contact.lastname + '; ' + contact.email + '</li>');
+        addContact(contact);
       });
+    },
+    error: function(){
+      alert:('Error loading contacts');
     }
+  });
+
+  $('#add-contact').on('click', function(){
+
+    var contact = {
+      firstname: $firstname.val(),
+      lastname: $lastname.val(),
+      email: $email.val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      dataType: 'json',
+      url: '/contacts',
+      data: contact,
+      success: function(newContact){
+        addContact(newContact);
+      },
+      error: function(){
+        alert:('Error creating contact');
+      }
+    });
+
   });
   
 });
